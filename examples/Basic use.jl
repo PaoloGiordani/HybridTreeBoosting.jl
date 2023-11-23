c6a276d8ec7167cd63664706be6cfa85eeef070b
Paolo Giordani <paolo.giordani@bi.no>
2023-11-23 10:25:18 +0100

    Add files via upload

---
+++ examples/Basic use.jl
@@ -0,0 +1,121 @@
+
+"""
+
+Short description:
+
+- Illustrates basic use on a regression problem.
+- param.modality as the most important user's choice.
+- In default modality, SMARTboost performs automatic hyperparameter tuning.
+
+
+Extensive description: 
+
+Simulated iid data, additively nonlinear dgp.
+
+- loss can be :L2 or :logistic or :Huber or :t (recommended in place of :Huber).
+   If :logistic, the fitted and forecast values are for the log odds ratio
+- fit, with automatic hyperparameter tuning if modality is :compromise or :accurate
+- save fitted model (upload fitted model)
+- feature importance
+- partial effects plots
+
+
+Options for SMARTboost: modality is the key parameter guiding hyperparameter tuning and learning rate.
+:fast and :fastest only fit one model at default parameters, while :compromise and :accurate perform
+automatic hyperparameter tuning. In SMARTboost, it is not recommended that the user performs 
+hyperparameter tuning by cross-validation, because this process is done automatically if modality is
+:compromise or :accurate. The recommended process is to first run in modality=:fast or :fastest,
+for exploratory analysis and to gauge computing time, and then switch to :compromise (default)
+or :accurate.
+
+paolo.giordani@bi.no
+"""
+
+number_workers  = 8  # desired number of workers
+
+using Distributed
+nprocs()<number_workers ? addprocs( number_workers - nprocs()  ) : addprocs(0)
+@everywhere using SMARTboostPrivate
+
+using Random,Plots 
+
+# USER'S OPTIONS 
+
+Random.seed!(123)
+
+# Some options for SMARTboost
+loss      = :L2           # :L2 or :logistic (or :Huber or :t). 
+modality  = :fast         # ::accurate, :compromise (default), :fast, :fastest 
+
+priortype = :hybrid       # :hybrid (default) or :smooth to force smoothness 
+nfold     = 1             # number of cv folds. 1 faster (single validation sets), default 5 is slower, but more accurate.
+nofullsample = true       # if nfold=1 and nofullsample=true, the model is not re-fitted on the full sample after validation of the number of trees
+
+randomizecv = false       # false (default) to use block-cv. 
+verbose     = :Off
+warnings    = :On
+ 
+# options to generate data. y = sum of four additive nonlinear functions + Gaussian noise.
+n,p,n_test  = 10_000,4,100_000
+stde        = 1.0
+  
+f_1(x,b)    = b*x .+ 1 
+f_2(x,b)    = sin.(b*x)  # for higher nonlinearities, try #f_2(x,b) = 2*sin.(2.5*b*x)
+f_3(x,b)    = b*x.^3
+f_4(x,b)    = b./(1.0 .+ (exp.(40.0*(x .- 0.5) ))) .- 0.1*b
+
+b1,b2,b3,b4 = 1.5,2.0,0.5,2.0
+
+# END USER'S OPTIONS
+
+# generate data
+x,x_test = randn(n,p), randn(n_test,p)
+
+f        = f_1(x[:,1],b1) + f_2(x[:,2],b2) + f_3(x[:,3],b3) + f_4(x[:,4],b4)
+f_test   = f_1(x_test[:,1],b1) + f_2(x_test[:,2],b2) + f_3(x_test[:,3],b3) + f_4(x_test[:,4],b4)
+
+loss==:logistic ? y = (exp.(f)./(1.0 .+ exp.(f))).>rand(n) : y=stde*randn(n)+f
+
+# set up SMARTparam and SMARTdata, then fit and predit
+param  = SMARTparam(loss=loss,priortype=priortype,randomizecv=randomizecv,nfold=nfold,verbose=verbose,warnings=warnings,
+           modality=modality,nofullsample=nofullsample)
+data   = SMARTdata(y,x,param)
+
+@time output = SMARTfit(data,param)
+yf     = SMARTpredict(x_test,output,predict=:Egamma)  # predict the natural parameter
+
+println(" \n modality = $(param.modality), nfold = $nfold ")
+println(" depth = $(output.bestvalue), number of trees = $(output.ntrees) ")
+println(" out-of-sample RMSE from truth ", sqrt(sum((yf - f_test).^2)/n_test) )
+
+# save (load) fitted model
+# using JLD2
+#@save "output.jld2" output
+#@load "output.jld2" output    # Note: key must be the same, e.g. @load "output.jld2" output2 is a KeyError
+
+# feature importance, partial dependence plots and marginal effects
+fnames,fi,fnames_sorted,fi_sorted,sortedindx = SMARTrelevance(output,data,verbose=false)
+q,pdp  = SMARTpartialplot(data,output,[1,2,3,4],predict=:Egamma)
+
+# plot partial dependence in terms of the natural parameter 
+pl   = Vector(undef,4)
+f,b  = [f_1,f_2,f_3,f_4],[b1,b2,b3,b4]
+
+for i in 1:length(pl)
+    pl[i]   = plot( [q[:,i]],[pdp[:,i] f[i](q[:,i],b[i]) - f[i](q[:,i]*0,b[i])],
+           label = ["smart" "dgp"],
+           legend = :bottomright,
+           linecolor = [:blue :red],
+           linestyle = [:solid :dot],
+
+           linewidth = [5 5],
+           titlefont = font(15),
+           legendfont = font(12),
+           xlabel = "x",
+           ylabel = "f(x)",
+           )
+end
+
+display(plot(pl[1], pl[2], pl[3], pl[4], layout=(2,2), size=(1300,800)))  # display() will show it in Plots window.
+
+
---
+++ examples/Speedups with sparsevs.jl
@@ -0,0 +1,143 @@
+
+"""
+
+Short description:
+
+- Explore how sparsevs affects speed and accuracy.
+
+Extensive description: 
+
+Sparsevs can be used to speed up SMARTboost with large number of features.
+The idea is to to store, at predetermined intervals (a Fibonacci sequence in default), the 
+ten (or other number: param.number_best_features) features that had the lowest loss in each
+split of the treed. For example, for a tree of depth 4, as many as 40 features will be stored
+in this group of best_features. In the next tree, only the features in this group will be
+considered as candidates for splitting, saving time for large p. At the next predetermined
+update, the best features are added to this group.
+Since features never leave the group of best_features, this group can get large if the environment
+is dense, and will stay small if the environment is sparse. Large speed-ups gains are therefore
+not guaranteed, but the forecasting accuracy should not be strongly affected except in extreme
+cases where several hundred features are needed to accurately fit the data. 
+
+paolo.giordani@bi.no
+"""
+
+number_workers  = 8  # desired number of workers
+
+using Distributed
+nprocs()<number_workers ? addprocs( number_workers - nprocs()  ) : addprocs(0)
+@everywhere using SMARTboostPrivate
+
+using Random
+using LightGBM
+
+# USER'S OPTIONS 
+
+Random.seed!(123)
+
+# Options for data generation 
+n         = 1_000
+p         = 1000        # number of features 
+dummies   = false       # true if x, x_test are 0-1 (faster).
+stde      = 1            
+
+# Options for SMARTboost: modality is the key parameter guiding hyperparameter tuning and learning rate.
+# :fast and :fastest only fit one model at default parameters, while :compromise and :accurate perform
+# automatic hyperparameter tuning. 
+
+modality         = :fast   # :accurate, :compromise (default), :fast, :fastest
+sparsevs         = :On      
+
+frequency_update = 1     # Integer. Default 1. >1 to update less frequently (larger speed gains, less precision)
+nfold            = 1     # 1 for fair comparison with LightGBM
+nofullsample     = true  # true for fair comparison with LightGBM
+
+verbose          = :Off
+warnings         = :On
+
+ntrees           = 1000   # maximum number of trees 
+
+# function: Friedman of linear, with pstar relevant features.
+# Increasing the number of relevant features increases the difficulty of the problem and can be
+# used to evaluate speed gains and accuracy losses of sparsevs.
+
+Friedman_function(x) = 10.0*sin.(π*x[:,1].*x[:,2]) + 20.0*(x[:,3].-0.5).^2 + 10.0*x[:,4] + 5.0*x[:,5]
+
+p_star    = 10       # number of relevant features 
+β = randn(p_star)
+Linear_function_Gaussian(x)   = x[:,1:length(β)]*β
+
+f_dgp     = Friedman_function     
+#f_dgp    = Linear_function_Gaussian
+
+# END USER'S INPUTS 
+
+
+if f_dgp==Friedman_function
+    x,x_test = rand(n,p), rand(200_000,p)    # Friedman function on U(0,1)
+else 
+    x,x_test = randn(n,p), randn(200_000,p)    
+
+    if dummies 
+        x,x_test = Float64.(x .> 0), Float64.(x_test .> 0)
+    end 
+end     
+
+
+f       = f_dgp(x)
+y      = f + stde*randn(n)
+f_true = f_dgp(x_test)
+
+# LightGBM
+
+# Create an estimator with the desired parameters—leave other parameters at the default values.
+estimator = LGBMRegression(   # LGBMRegression(...)
+    objective = "regression",
+    categorical_feature = [],  # or [1,2,3,5,6,7,8], treating date as a category, as probably Lightgbm would.
+    num_iterations = ntrees,   # default 100
+    learning_rate = 0.1,    # default 0.1
+    early_stopping_round = 50,  # default 0, i.e. Inf
+    bagging_fraction = 1.0,
+    feature_fraction = 1.0,
+    metric = ["l2"],
+    num_threads = number_workers,
+    max_depth = -1,         # -1 for default
+    #device_type="cpu",
+    #max_depth = -1   # no limit
+)
+
+sharevalidation = 0.3
+n_train = Int(round((1-sharevalidation)*length(y)))
+x_train = x[1:n_train,:]; y_train = y[1:n_train]
+x_val   = x[n_train+1:end,:]; y_val = y[n_train+1:end]
+
+LightGBM.fit!(estimator,x_train,y_train,(x_val,y_val),verbosity=-1)
+yf_gbm = LightGBM.predict(estimator,x_test)
+
+
+# SMARtboost
+
+param   = SMARTparam(modality=modality,ntrees=ntrees,n_vs=n_vs,n_preliminaryvs=n_preliminaryvs,nofullsample=nofullsample,
+                    preliminaryvs=preliminaryvs,priortype=priortype,sparsevs=sparsevs,frequency_update=frequency_update,
+                    nfold=nfold,verbose=:Off,warnings=:On)
+
+data  = SMARTdata(y,x,param)
+
+println("\n n = $n, p = $p, dummies=$dummies, modality = $modality, sparsevs = $sparsevs, frequency_update = $frequency_update")
+println(" time to fit ")
+
+@time output = SMARTfit(data,param);
+yf = SMARTpredict(x_test,output,predict=:Ey)  # predict
+
+println("\n RMSE of SMARTboost from true E(y|x)                   ", sqrt(mean((yf-f_true).^2)) )
+println(" RMSE of LightGBM (default param) from true E(y|x)     ", sqrt(mean((yf_gbm-f_true).^2)) )
+
+fnames,fi,fnames_sorted,fi_sorted,sortedindx = SMARTrelevance(output,data,verbose=false);
+println(" SMARTboost number of included features in final model $(sum(fi.>0))")
+
+if param.sparsevs == :On
+    @info " sparsevs is :On. Repeat with sparsevs=:Off to track speed gains and any accuracy loss. Speed gains will be smaller if many features are relevant."
+else 
+    @info " sparsevs is :Off. Repeat with sparsevs=:On to track speed loss and any accuracy gains. Speed gains will be smaller if many features are relevant."
+end     
+
