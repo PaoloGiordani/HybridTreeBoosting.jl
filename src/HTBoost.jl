1946c2ba414bb513b3c1c10327bebb6aa9618b17
Paolo Giordani <paolo.giordani@bi.no>
2024-03-13 09:42:51 +0100

    src/HTBoost, since name has changed

---
+++ src/HTBoost.jl
@@ -0,0 +1,26 @@
+module HTBoost
+
+export HTBinfo, HTBloglikdivide, HTBparam, HTBdata, HTBfit, HTBpredict, HTBrelevance, HTBpartialplot,
+ HTBmarginaleffect, HTBtree, HTBoostTrees, HTBindexes_from_dates, losscv, HTBweightedtau, HTBmulticlass_loss,
+ HTBcoeff 
+
+using Distributed, SharedArrays, LinearAlgebra,Statistics, DataFrames,Dates, Random
+using CategoricalArrays, Base.Threads
+import Optim, LineSearches, SpecialFunctions
+
+include("param.jl")
+include("categorical.jl")
+include("data_preparation.jl")
+include("initialize.jl")
+include("loss.jl")
+include("struct.jl")                 
+include("fit.jl")
+include("coefficients_estimate.jl")
+include("validation.jl")
+include("preliminary_cv.jl")
+include("main_functions.jl")          
+include("print_table_ps.jl")          # Paul Soderlind's function for nice output printing. Use PrettyTables instead?
+include("model_combination.jl")
+
+
+end 
